118e66d0388ed61f757f557925781fa3e8e7e10b
Mars <mars@marsnix.kane.net>
2025-11-20 22:07:26 -0600

    sync for the night

--- boilermaker_core/src/commands/search.rs
+++ boilermaker_core/src/commands/search.rs
@@ -1,41 +1,19 @@
 use clap::Parser;
 use color_eyre::Result;
-// use tabled::{Table, settings::Style};
-// use tracing::info;
 
-// use crate::db::template_cache::DisplayableTemplateListResult;
 use crate::state::AppState;
 
 #[derive(Parser)]
 pub struct Search {
-    #[arg(short = 'u', long)]
-    pub public: bool,
-    #[arg(short = 'p', long)]
-    pub private: bool,
+    #[arg(required = true, help = "Search query (regex supported)")]
+    pub query: String,
+    #[arg(short = 'l', long, help = "Search only installed templates")]
+    pub local: bool,
+    #[arg(short = 's', long, help = "Search a specific source")]
+    pub src: Option<String>,
 }
 
 pub async fn search(_app_state: &AppState, _cmd: &Search) -> Result<()> {
-    /*
-    let cache = app_state.template_db.clone();
-
-    let result = cache.list_templates(None).await?;
-
-    let rows = result
-        .into_iter()
-        .map(DisplayableTemplateListResult::to_std_row)
-        .collect::<Vec<_>>();
-
-    if rows.is_empty() {
-        info!("No templates found in the cache.");
-        info!("💡 Have a look at `boil install`");
-        return Ok(());
-    }
-
-    let mut table = Table::new(&rows);
-    table.with(Style::psql());
-
-    print!("\n\n{table}\n\n");
-     */
-
+    println!("TODO: PICKUP HERE - search command not yet implemented");
     Ok(())
 }
