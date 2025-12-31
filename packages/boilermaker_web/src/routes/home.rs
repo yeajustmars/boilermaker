67cb66250a78e4d433a703bd8898fa0d9aa90d2e
Mars <yeajustmars@gmail.com>
2025-12-31 14:38:45 -0600

    web (unstable): syncing changes

--- packages/boilermaker_web/src/routes/home.rs
+++ packages/boilermaker_web/src/routes/home.rs
@@ -2,11 +2,18 @@ use std::sync::Arc;
 
 use axum::{extract::State, http::StatusCode, response::Html};
 use axum_template::TemplateEngine;
+use dioxus::asset_resolver::read_asset_bytes;
 use minijinja::context;
 
 use crate::WebAppState;
+use boilermaker_ui::MAIN_CSS;
 
 pub async fn home(State(app): State<Arc<WebAppState>>) -> Result<Html<String>, StatusCode> {
+    let main_css = String::from_utf8(read_asset_bytes(&MAIN_CSS).await.unwrap()).unwrap();
+    println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+    println!("MAIN_CSS: {MAIN_CSS:?}");
+    println!("main_css: {main_css:?}");
+    println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
     let page = app
         .template
         .render("home.html", context! {})
