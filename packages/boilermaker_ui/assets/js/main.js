218b76386945b257495febb2a65f24eac72a8fec
yeajustmars <yeajustmars@gmail.com>
2026-01-22 11:07:54 -0600

    web: add Collapsibles for accordion

--- packages/boilermaker_ui/assets/js/main.js
+++ packages/boilermaker_ui/assets/js/main.js
@@ -12,6 +12,7 @@
   };
 
   document.addEventListener("DOMContentLoaded", function() {
+    // ________________________________________ Main dropdown navigation
     const mainMenu = document.querySelector("#nav-main-dropdown-button");
     const mainDropdown = document.querySelector("#nav-main-dropdown-menu");
 
@@ -26,6 +27,20 @@
       hideElement(mainDropdown);
     });
 
+    // ________________________________________ Collapsibles
+    const collapsibleButtons = document.querySelectorAll(".collapsible-link");
+    collapsibleButtons.forEach(button => {
+      button.addEventListener("click", (event) => {
+        let content = event.target.closest(".collapsible").querySelector(".collapsible-content");
+        if (content.classList.contains("hidden")) {
+          content.classList.remove("hidden");
+        } else {
+          content.classList.add("hidden");
+        }
+      })
+    });
+
+    // ________________________________________ Code
     hljs.highlightAll();
   });
 
