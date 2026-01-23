34e2f59839786e15c5aa24ccd8a6955bb1e8946c
yeajustmars <yeajustmars@gmail.com>
2026-01-23 15:39:22 -0600

    core: add sources.templates.install

--- packages/boilermaker_core/src/commands/sources/templates/install.rs
+++ packages/boilermaker_core/src/commands/sources/templates/install.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
 use color_eyre::{Result, eyre::eyre};
 
-use crate::{db::SourceTemplateFindParams, state::AppState, util::help};
+use crate::{
+    commands::install::{Install as CoreCmdInstall, install as core_cmd_install},
+    db::{SourceTemplateFindParams, SourceTemplateResult},
+    state::AppState,
+    util::help,
+};
 
 #[derive(Debug, Parser)]
 pub struct Install {
@@ -12,14 +17,14 @@ pub struct Install {
 }
 
 #[tracing::instrument]
-pub async fn install(app_state: &AppState, cmd: &Install) -> Result<()> {
-    /*
-    let st = if let Ok(id) = cmd.id_or_name.parse::<i64>() {
-        let cache = app_state.local_db.clone();
+async fn get_source_template(app_state: &AppState, cmd: &Install) -> Result<SourceTemplateResult> {
+    let cache = app_state.local_db.clone();
+
+    if let Ok(id) = cmd.id_or_name.parse::<i64>() {
         Ok(cache
             .get_source_template(id)
             .await?
-            .expect("Failed to unwrap source template result"))
+            .expect("Failed to unwrap template result"))
     } else {
         let find_params = SourceTemplateFindParams {
             ids: None,
@@ -34,24 +39,41 @@ pub async fn install(app_state: &AppState, cmd: &Install) -> Result<()> {
         let results = cache.find_source_templates(find_params).await?;
 
         match results.len() {
-            0 => Err(eyre!("💥 Cannot find template: {}.", cmd.id_or_name))?,
+            0 => Err(eyre!("💥 Cannot find source template: {}.", cmd.id_or_name))?,
             1 => Ok(results
                 .first()
                 .unwrap_or(Err(eyre!(
-                    "💥 Cannot retrieve template: {}.",
+                    "💥 Cannot retrieve source template: {}.",
                     cmd.id_or_name
                 ))?)
                 .to_owned()),
             2.. => {
-                help::print_multiple_template_results_help(&results);
+                help::print_multiple_source_template_results_help(&results);
                 Err(eyre!(
-                    "💥 Found multiple results matching template: {}.",
+                    "💥 Found multiple results matching source template: {}.",
                     cmd.id_or_name
                 ))?
             }
         }
+    }
+}
+
+#[tracing::instrument]
+pub async fn install(app_state: &AppState, cmd: &Install) -> Result<()> {
+    let st = get_source_template(app_state, cmd).await?;
+
+    let cmd = CoreCmdInstall {
+        template: st.repo,
+        rename: Some(st.name),
+        lang: Some(st.lang),
+        branch: st.branch,
+        subdir: st.subdir,
+        local: false,
     };
-     */
+
+    if let Err(e) = core_cmd_install(app_state, &cmd).await {
+        return Err(eyre!("Could not install template from source: {e}"));
+    }
 
     Ok(())
 }
