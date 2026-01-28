a7910228e89ed5ed6c886707c4183ef46adc66cf
yeajustmars <yeajustmars@gmail.com>
2026-01-28 01:31:02 -0600

    web: add template details page, files feature stub
    - and other misc. things around these

--- packages/boilermaker_web/src/routes/mod.rs
+++ packages/boilermaker_web/src/routes/mod.rs
@@ -2,10 +2,12 @@ mod docs;
 mod get_involved;
 mod home;
 mod settings;
+mod template;
 mod templates;
 
 pub use docs::*;
 pub use get_involved::*;
 pub use home::*;
 pub use settings::*;
+pub use template::*;
 pub use templates::*;
