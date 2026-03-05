8c0680f66ead276956fb4dec1fadc0327700f081
yeajustmars <yeajustmars@gmail.com>
2026-03-04 21:00:00 -0600

    web,docs: add missing route stubs, tweak docs

--- packages/boilermaker_web/src/routes/get_involved.rs
+++ packages/boilermaker_web/src/routes/get_involved.rs
@@ -1,11 +1,18 @@
 use std::sync::Arc;
 
 use axum::{extract::State, http::StatusCode, response::Html};
+use axum_template::TemplateEngine;
+use color_eyre::eyre::Result;
+use minijinja::context;
 
-use crate::WebAppState;
+use crate::{WebAppState, make_context};
 
-pub async fn get_involved(
-    State(_app): State<Arc<WebAppState>>,
-) -> Result<Html<String>, StatusCode> {
-    Ok(Html("<h1>Get Involved</h1>".to_string()))
+#[tracing::instrument]
+pub async fn get_involved(State(app): State<Arc<WebAppState>>) -> Result<Html<String>, StatusCode> {
+    let ctx = make_context(context! { title => "Get Involved", });
+    let out = app
+        .template
+        .render("get_involved.html", ctx)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Html(out))
 }
