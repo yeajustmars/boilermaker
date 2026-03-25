e2bb10ea992c7716c4683eddee8b2b06ad0b03c7
yeajustmars <yeajustmars@gmail.com>
2026-03-24 19:58:00 -0600

    rm `boil config set` for now

--- packages/boilermaker_core/src/commands/config/mod.rs
+++ packages/boilermaker_core/src/commands/config/mod.rs
@@ -3,13 +3,13 @@ use clap::Subcommand;
 pub mod get;
 pub use get::{Get, get};
 
-pub mod set;
-pub use set::{Set, set};
+//pub mod set;
+//pub use set::{Set, set};
 
 #[derive(Subcommand)]
 pub enum Config {
     #[command(about = "Get Boilermaker config")]
     Get(Get),
-    #[command(about = "Set Boilermaker config")]
-    Set(Set),
+    //#[command(about = "Set Boilermaker config")]
+    //Set(Set),
 }
