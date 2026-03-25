dbdf5f08c89c7b383344ef30309ce5dad5bea5b3
yeajustmars <yeajustmars@gmail.com>
2026-03-24 19:27:39 -0600

    core: shorten repo str in table output

--- packages/boilermaker_core/src/util/output.rs
+++ packages/boilermaker_core/src/util/output.rs
@@ -1,6 +1,8 @@
 use tabled::{Table, Tabled, settings::Style};
 use tracing::error;
 
+use crate::constants::URL_PREFIX_PATTERN;
+
 pub fn print_table<I, T>(rows: I)
 where
     I: IntoIterator<Item = T>,
@@ -23,4 +25,8 @@ where
         Some(msg) => error!("{}\n\n{table}\n", msg),
         None => error!("\n{table}\n"),
     }
+}
+
+pub fn strip_url_prefix(s: &str) -> String {
+    URL_PREFIX_PATTERN.replace(s, "").to_string()
 }
